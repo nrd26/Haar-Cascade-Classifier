089815dd7e6f4a56d09ac20b47637e21f35cc8f7
Nihal Rian Dias <nihaloma26@gmail.com>
2020-07-19 02:37:20 +0530

    Update cats.py

--- harr_cascade_classifier/cats/cats.py
+++ harr_cascade_classifier/cats/cats.py
@@ -1,25 +1,25 @@
 import numpy as np
-import cv2
+import cv2 as cv
 
 
-cat_cascade = cv2.CascadeClassifier('cats.xml')
-cap = cv2.VideoCapture(0)
+cat_cascade = cv.CascadeClassifier('cats.xml')
+cap = cv.VideoCapture(0)
 
 while 1:
     ret, img = cap.read()
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     
     cats = cat_cascade.detectMultiScale(gray, 1.3, 5)
     for (x, y, w, h) in cats:
-        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         roi = gray[y:y + h, x:x + w]
 
 
-    cv2.imshow('img', img)
-    if cv2.waitKey(1) & 0xFF==ord('q'):
+    cv.imshow('img', img)
+    if cv.waitKey(1) & 0xFF==ord('q'):
         break
 
 cap.release()
-cv2.destroyAllWindows()
+cv.destroyAllWindows()
 
 
